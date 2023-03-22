3b219fcb4601d39b59d472e2af13d6ec8e8f2bbf
Jaskirat <jaskiratr@gmail.com>
2023-03-22 17:58:53 -0600

    4b57352

---
+++ sw.js
@@ -0,0 +1,37 @@
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.js')
+
+// --------------------------------------------------
+// Configure
+// --------------------------------------------------
+
+// Set workbox config
+workbox.setConfig({
+  "debug": false
+})
+
+// Start controlling any existing clients as soon as it activates
+workbox.core.clientsClaim()
+
+// Skip over the SW waiting lifecycle stage
+workbox.core.skipWaiting()
+
+workbox.precaching.cleanupOutdatedCaches()
+
+// Enable offline Google Analytics tracking
+workbox.googleAnalytics.initialize()
+
+// --------------------------------------------------
+// Precaches
+// --------------------------------------------------
+
+// Precache assets
+
+// --------------------------------------------------
+// Runtime Caching
+// --------------------------------------------------
+
+// Register route handlers for runtimeCaching
+workbox.routing.registerRoute(new RegExp('/\/article/'), new workbox.strategies.StaleWhileRevalidate ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'), new workbox.strategies.StaleWhileRevalidate ({"cacheName":"google-fonts","cacheExpiration":{"maxEntries":30,"maxAgeSeconds":300}}), 'GET')
+workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
